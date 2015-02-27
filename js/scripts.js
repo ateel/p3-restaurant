7c38412da7e3b101c7c3dcbb0f79854731a5f985
ateel <ashley.teel@eagles.ewu.edu>
2015-02-27 12:27:37 -0800

    menu styling, hover descriptions

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@ $(function() {
     $("section:not(.raw)").addClass("fadey");      
   });
 });
+
+
+$(function() {                       
+  $("#SpicyLink").click(function() {  
+    $("section:not(.spicy)").addClass("fadey");      
+  });
+});
+
+$(function() {                       
+  $("#VeggieFood").click(function() {  
+    $("section:not(.veg)").addClass("fadey");      
+  });
+});
